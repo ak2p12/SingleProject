61007bea20ce7a7f2aebc5b84ab97cc36ddfa37e
ak2p12 <ak2p12@gmail.com>
2020-10-22 00:19:16 +0900

    마우스위치에따른 캐릭터 회전 구현 움직임에 따른 애니메이션 구현중

--- Single/Assets/Scripts/UserController.cs
+++ Single/Assets/Scripts/UserController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class UserController : MonoBehaviour
 {
     private UserMain userMain;
     private Camera userCamera;
+    private Ray ray;
+    private RaycastHit rayHit;
+    private Animator animatorController;
+
+    private float axisX;
+    private float axisZ;
 
     void Start()
     {
         userMain = GetComponent<UserMain>();
+        animatorController = GameObject.Find("User").GetComponentInChildren<Animator>();
         userCamera = GameObject.Find("UserCamera").GetComponent<Camera>();
         StartCoroutine(Input_Coroutine());
     }
@@ -18,34 +26,52 @@ public class UserController : MonoBehaviour
     {
         while(true)
         {
-            if(Input.GetKey(KeyCode.W))
-            {
-                transform.Translate(
-                    Vector3.forward * userMain.GetUnitInfomation().MoveSpeed  * Time.deltaTime,
-                    Space.World);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(
-                    Vector3.left * userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
-                    Space.World);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(
-                    Vector3.back * userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
-                    Space.World);
-            }
+            axisX = Input.GetAxis("Horizontal");
+            axisZ = Input.GetAxis("Vertical");
+            animatorController.SetFloat("Dir X", axisX);
+            animatorController.SetFloat("Dir Z", axisZ);
+
+            //오른쪽
             if (Input.GetKey(KeyCode.D))
             {
                 transform.Translate(
-                    Vector3.right * userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
+                    Vector3.right * axisX * userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
+                    Space.World);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(
+                    Vector3.left * axisX * -userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
+                    Space.World);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(
+                    Vector3.forward * axisZ * userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
+                    Space.World); ;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(
+                    Vector3.back * axisZ * -userMain.GetUnitInfomation().MoveSpeed * Time.deltaTime,
                     Space.World);
             }
 
             Vector3 mousePos = Input.mousePosition;
-            Debug.Log(userCamera.ScreenPointToRay(mousePos).ToString());
+            
+            ray = userCamera.ScreenPointToRay(mousePos);
 
+            if ( Physics.Raycast(ray , out rayHit, 50.0f) )
+            {
+                transform.LookAt(rayHit.point);
+            }
+
+            //Debug.Log("axisX : " + axisX.ToString());
+            //Debug.Log("Vector3.left * axisX : " + (Vector3.left * axisX).ToString() );
+            
+            //Debug.Log(Input.GetAxis("Vertical").ToString());
+            
             yield return null;
         }
     }
